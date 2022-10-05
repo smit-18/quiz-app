7b173a3d5424b0087f6ccbec8136641e0366a066
Smit Vekaria <smit.vce19@sot.pdpu.ac.in>
2022-10-05 16:21:57 +0530

    Updates

---
+++ precache-manifest.c33ab38cba0b2af0660476e0afa859d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94ba93f0bc1533e79fe010fd373c5fc",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "2514b7492b0102b23718",
+    "url": "/quiz-app/static/css/main.9f612b33.chunk.css"
+  },
+  {
+    "revision": "7889a0e298549cb854ad",
+    "url": "/quiz-app/static/js/2.bb7ee05c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.bb7ee05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2514b7492b0102b23718",
+    "url": "/quiz-app/static/js/main.5f40d628.chunk.js"
+  },
+  {
+    "revision": "cc09c093cf6c73f0a177",
+    "url": "/quiz-app/static/js/runtime-main.2aa3188d.js"
+  }
+]);
